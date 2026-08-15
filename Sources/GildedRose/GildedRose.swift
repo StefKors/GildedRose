8ff280c75cabe439aae79a9b455030232a45498b
Stef Kors <stef.kors@gmail.com>
2026-08-15 12:49:05 +0200

    Pull up repeated behaviour into methods & match on categories

--- Sources/GildedRose/GildedRose.swift
+++ Sources/GildedRose/GildedRose.swift
@@ -1,3 +1,40 @@
+extension Item {
+    enum Category {
+        case sulfuras
+        case brie
+        case backstage
+        case normalItems
+    }
+
+    var isBrie: Bool {
+        self.category == .brie
+    }
+
+    var isBackstage: Bool {
+        self.category == .backstage
+    }
+
+    var isSulfuras: Bool {
+        self.category == .sulfuras
+    }
+
+    var isNormalItem: Bool {
+        self.category == .normalItems
+    }
+
+    var category: Category {
+        if self.name == "Aged Brie" {
+            return .brie
+        } else if self.name == "Backstage passes to a TAFKAL80ETC concert" {
+            return .backstage
+        } else if self.name == "Sulfuras, Hand of Ragnaros" {
+            return .sulfuras
+        } else {
+            return .normalItems
+        }
+    }
+}
+
 public class GildedRose {
     var items: [Item]
 
@@ -5,55 +42,66 @@ public class GildedRose {
         self.items = items
     }
 
+    fileprivate func improveQuality(_ item: Item) {
+        if item.quality < 50 {
+            item.quality = item.quality + 1
+        }
+    }
+    
+    fileprivate func reduceQuality(_ item: Item) {
+        if item.quality > 0 {
+            item.quality = item.quality - 1
+        }
+    }
+    
+    fileprivate func reduceSellIn(_ item: Item) {
+        item.sellIn = item.sellIn - 1
+    }
+    
     public func updateQuality() {
-        for i in 0 ..< items.count {
-            if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                if items[i].quality > 0 {
-                    if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if items[i].quality < 50 {
-                    items[i].quality = items[i].quality + 1
+        for item in items {
+            reduceSellIn(item)
 
-                    if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].sellIn < 11 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
+            if item.isSulfuras {
+                // do nothing
+            }
 
-                        if items[i].sellIn < 6 {
-                            if items[i].quality < 50 {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
+            if item.isBrie {
+                improveQuality(item)
+
+                if item.sellIn < 0 {
+                    improveQuality(item)
                 }
             }
 
-            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                items[i].sellIn = items[i].sellIn - 1
-            }
+            if item.isBackstage {
+                improveQuality(item)
 
-            if items[i].sellIn < 0 {
-                if items[i].name != "Aged Brie" {
-                    if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-                        if items[i].quality > 0 {
-                            if items[i].name != "Sulfuras, Hand of Ragnaros" {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if items[i].quality < 50 {
-                        items[i].quality = items[i].quality + 1
-                    }
+                // sellin after reduce
+                if item.sellIn < 10 {
+                    improveQuality(item)
+                }
+
+                // sellin after reduce
+                if item.sellIn < 5 {
+                    improveQuality(item)
+                }
+
+                // sellin after reduce
+                if item.sellIn < 0 {
+                    item.quality = 0
                 }
             }
+
+            if item.category != .brie && item.category != .backstage {
+                reduceQuality(item)
+
+                if item.sellIn < 0 {
+                    reduceQuality(item)
+                }
+            }
+
+
         }
     }
 }
